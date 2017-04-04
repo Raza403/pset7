f503143979cdd21ecebd3b1e9c92871330d26aa8
raza403 <raza403@gmail.com>
2017-04-04 14:43:58 +0000

    buy is working perfect now have to work on index,shares not shown on index page

--- finance/application.py
+++ finance/application.py
@@ -91,12 +91,11 @@ def buy():
             ide = session["user_id"]
            
             csh = db.execute("SELECT * FROM users WHERE id = :ide", ide = ide)
-            #Only go forward if user have enough money
+            #Only go forward if user have enough
             if prc <= csh[0]["cash"]:
                 db.execute("INSERT INTO portfolio (id, symbol,price,shares) VALUES (:ide, :symbol, :price, :shares)", ide = ide,symbol = symbl, price = prc, shares = number)
                 db.execute("UPDATE users SET cash = :cash WHERE id = :ide",cash = csh[0]["cash"] - prc, ide = ide)
                 return render_template("index.html")
-                #here I have to change the code for update
             else:
                 return apology ("You don't have enough cash to buy these stocks")
     else:
